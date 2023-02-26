085151fa0fd3770fb92da2885255720b9fed0ade
Thomas Mielke <thomas.mielke@c3s.cc>
2023-02-26 15:37:51 +0100

    "moved debugger ports from 5100X to 5200X\n\nunder Windows wit WSL, Hyper-V was reserving some 51000er ports"

--- API/api.py
+++ API/api.py
@@ -72,10 +72,10 @@ class query:
 class delete:
     def POST(self):
         import ptvsd  # unconditional import breaks test coverage
-        ptvsd.enable_attach(address=("0.0.0.0", 51004),
+        ptvsd.enable_attach(address=("0.0.0.0", 52004),
                             redirect_output=True)
         ptvsd.wait_for_attach()
-        ptvsd.break_into_debugger()        
+        ptvsd.break_into_debugger()
         return self.GET()
 
     def GET(self):
